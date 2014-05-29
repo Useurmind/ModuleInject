0594979b65b6892c95973fcfefa46064d66c4c65
Jochen Grün <gruen@intel-vci.uni-saarland.de>
2014-05-29 20:54:56 +0200

    Missing files added.

---
+++ Test.ModuleInject/TestModules/BehaviourModule.cs
@@ -0,0 +1,35 @@
+﻿using ModuleInject;
+using ModuleInject.Fluent;
+using ModuleInject.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Test.ModuleInject.TestModules
+{
+    public interface IBehaviourModule : IInjectionModule
+    {
+        IMainComponent1 InterceptedWithChangeReturnValueComponent { get; }
+    }
+
+    public class BehaviourModule : InjectionModule<IBehaviourModule, BehaviourModule>, IBehaviourModule
+    {
+        public IMainComponent1 InterceptedWithChangeReturnValueComponent { get; set; }
+
+        public BehaviourModule()
+        {
+        }
+
+        public void RegisterBehaviour()
+        {
+            RegisterPublicComponent<IMainComponent1, MainComponent1>(x => x.InterceptedWithChangeReturnValueComponent)
+                .AddBehaviour<ChangeReturnValueBehaviour>();
+        }
+
+        public new void ActivateInterception()
+        {
+            base.ActivateInterception();
+        }
+    }
+}
