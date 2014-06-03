1826dc48ba5ba0c386c626e4765fd800afdc2c9b
Jochen Grün <jochen.gruen@googlemail.com>
2014-06-03 22:22:24 +0200

    Added missing file.

---
+++ ModuleInject/Fluent/ValueInjectionContextGeneric.cs
@@ -0,0 +1,23 @@
+﻿using ModuleInject.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ModuleInject.Fluent
+{
+    public class ValueInjectionContext<IComponent, TComponent, IModule, TModule, TValue>
+        where TComponent : IComponent
+        where TModule : IModule
+        where IModule : IInjectionModule
+    {
+        public ComponentRegistrationContext<IComponent, TComponent, IModule, TModule> ComponentContext { get; set; }
+        public TValue Value { get; private set; }
+
+        public ValueInjectionContext(ComponentRegistrationContext<IComponent, TComponent, IModule, TModule> componentContext, TValue value)
+        {
+            ComponentContext = componentContext;
+            Value = value;
+        }
+    }
+}
