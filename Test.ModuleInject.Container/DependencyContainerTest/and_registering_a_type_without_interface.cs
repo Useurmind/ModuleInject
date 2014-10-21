a401230cc5d5974e2d4d44ae9f764db2322ee34a
Jochen Grün <jochen.gruen@deutschebahn.com>
2014-10-21 23:25:24 +0200

    Started implementation on container.

---
+++ Test.ModuleInject.Container/DependencyContainerTest/and_registering_a_type_without_interface.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Test.ModuleInject.Container.DependencyContainerTest
+{
+    using NSpec;
+
+    class TestClass
+    {
+        
+    }
+
+    class and_registering_a_type : when_using_the_DependencyContainer
+    {
+        void before_each()
+        {
+            
+        }
+
+        void without_interface()
+        {
+            beforeEach = () => this.container.Register<TestClass>();
+
+            it["the registration is visible in the container"] = () => container.Registrations.Count().should_be(1);
+            it["the registration is for the registered type"] =
+                () => container.Registrations.ElementAt(0).ActualType.should_be(typeof(TestClass));
+        }
+    }
+}
---
+++ Test.ModuleInject.Container/DependencyContainerTest/when_using_the_DependencyContainer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Test.ModuleInject.Container.DependencyContainerTest
+{
+    using NSpec;
+
+    class when_using_the_DependencyContainer : nspec
+    {
+        protected DependencyContainer container;
+
+        void before_each()
+        {
+            container = new DependencyContainer();
+        }
+    }
+}
