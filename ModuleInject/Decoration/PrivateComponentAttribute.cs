1af9514dc6ac184efc185f7a911c64f51916a711
Jochen Grün <jochen.gruen@googlemail.com>
2014-11-29 22:50:20 +0100

    Moved some files, some refactoring, some comments.

--- ModuleInject/Decoration/PrivateComponentAttribute.cs
+++ ModuleInject/Decoration/PrivateComponentAttribute.cs
@@ -4,6 +4,17 @@ namespace ModuleInject.Decoration
 {
     using System;
 
+    /// <summary>
+    /// Used to mark private components.
+    /// Private components are the counterpart to public components.
+    /// Public components are part of the interface of the module and must not to be marked explicitly.
+    /// Private components on the other hand are not part of the interface and must be marked with this
+    /// attribute in the module implementation.
+    /// </summary>
+    /// <remark>
+    /// A private component does not need to have a private access modifier.
+    /// Access Modifiers for setters can be set at your own discretion.
+    /// </remark>
     [AttributeUsage(AttributeTargets.Property, Inherited = false, AllowMultiple = false)]
     public sealed class PrivateComponentAttribute : Attribute
     {
--- ModuleInject/Decoration/RegistryComponentAttribute.cs
+++ ModuleInject/Decoration/RegistryComponentAttribute.cs
@@ -4,6 +4,12 @@ namespace ModuleInject.Decoration
 {
     using System;
 
+    /// <summary>
+    /// States that a component is taken from the registry of the module.
+    /// For this type of component no registration is allowed in the module.
+    /// The components interface is automatically resolved from the registry 
+    /// and an error is thrown if the registry does not contain a matching registration.
+    /// </summary>
     [AttributeUsage(AttributeTargets.Property, Inherited = false, AllowMultiple = false)]
     public sealed class RegistryComponentAttribute : Attribute
     {
