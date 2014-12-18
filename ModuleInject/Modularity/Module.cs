cddf2e35dd775f9595d761ff4706a3d701535bc0
Jochen Grün <jochen.gruen@deutschebahn.com>
2014-12-19 00:59:12 +0100

    Added missing files.

---
+++ ModuleInject/Modularity/Module.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using ModuleInject.Common.Exceptions;
+using ModuleInject.Interfaces;
+using ModuleInject.Modularity.Registry;
+using ModuleInject.Modules;
+
+namespace ModuleInject.Modularity
+{
+    /// <summary>
+    /// Base class for all modules
+    /// </summary>
+    public abstract class Module : IModule, IDisposable
+    {
+        private IRegistry registry;
+
+        private bool isResolved;
+
+        /// <inheritdoc />
+        public bool IsResolved
+        {
+            get
+            {
+                return this.isResolved;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the registry.
+        /// </summary>
+        /// <value>
+        /// The registry.
+        /// </value>
+        public IRegistry Registry
+        {
+            get
+            {
+                if (this.registry as EmptyRegistry != null)
+                {
+                    return null;
+                }
+
+                return this.registry;
+            }
+            set
+            {
+                if (value == null)
+                {
+                    this.registry = new EmptyRegistry();
+                }
+                this.registry = value;
+            }
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Module"/> class.
+        /// </summary>
+        public Module()
+        {
+            this.registry = new EmptyRegistry();
+        }
+
+        /// <inheritdoc />
+        public void Resolve()
+        {
+            this.Resolve(null);
+        }
+
+        /// <inheritdoc />
+        public void Resolve(IRegistry parentRegistry)
+        {
+            if (this.IsResolved)
+            {
+                ExceptionHelper.ThrowTypeException(this.GetType(), Errors.InjectionModule_AlreadyResolved);
+            }
+
+            this.OnResolving();
+
+            var usedRegistry = this.GetUsedRegistry(parentRegistry);
+
+            RegistryResolver resolver = new RegistryResolver(this, usedRegistry);
+
+            resolver.Resolve();
+
+            this.OnRegistryResolved(usedRegistry);
+
+            this.isResolved = true;
+
+            this.OnResolved();
+        }
+
+        /// <summary>
+        /// Called when all registry components are resolved and in place.
+        /// Override it to implement the resolution logic for the module.
+        /// </summary>
+        protected abstract void OnRegistryResolved(IRegistry usedRegistry);
+
+        /// <summary>
+        /// Called before the <see cref="Resolve"/> method is executed.
+        /// </summary>
+        protected virtual void OnResolving() { }
+
+        /// <summary>
+        /// Called after the <see cref="Resolve"/> method was executed.
+        /// </summary>
+        protected virtual void OnResolved() { }
+
+        protected IRegistry GetUsedRegistry(IRegistry parentRegistry)
+        {
+            var usedRegistry = this.registry;
+            if (parentRegistry != null)
+            {
+                usedRegistry = this.registry.Merge(parentRegistry);
+            }
+            return usedRegistry;
+        }
+
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
+        public void Dispose()
+        {
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        /// Releases unmanaged and - optionally - managed resources.
+        /// </summary>
+        /// <param name="disposing"><c>true</c> to release both managed and unmanaged resources; <c>false</c> to release only unmanaged resources.</param>
+        protected virtual void Dispose(bool disposing)
+        {
+            this.registry.Dispose();
+        }
+    }
+}
---
+++ ModuleInject/Modularity/RegistryResolver.cs
@@ -0,0 +1,105 @@
+﻿using System.Reflection;
+
+using ModuleInject.Common.Exceptions;
+using ModuleInject.Common.Linq;
+using ModuleInject.Decoration;
+using ModuleInject.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using ModuleInject.Utility;
+
+namespace ModuleInject.Modularity
+{
+    /// <summary>
+    /// Finds all properties of the module marked with the <see cref="RegistryComponentAttribute"/>
+    /// and tries to retrieve them from the registry given to it.
+    /// If the properties contain modules they are resolved too with the given registry.
+    /// </summary>
+    public class RegistryResolver
+    {
+        private Module module;
+        private IRegistry usedRegistry;
+        private Type moduleType;
+
+        public RegistryResolver(Module module, IRegistry usedRegistry)
+        {
+            this.module = module;
+            this.moduleType = module.GetType();
+            this.usedRegistry = usedRegistry;
+        }
+
+        /// <summary>
+        /// Resolves the registry components in the module.
+        /// </summary>
+        public void Resolve()
+        {
+            var submodulePropertyInfos =
+                this.moduleType.GetModuleComponentPropertiesRecursive(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic)
+                               .Where(
+                                    p =>
+                                    {
+                                        bool isRegistry = p.HasCustomAttribute<RegistryComponentAttribute>();
+                                        return isRegistry;
+                                    });
+
+            foreach (var submodulePropInfo in submodulePropertyInfos)
+            {
+                TryResolveComponent(submodulePropInfo);
+
+                if (submodulePropInfo.IsInjectionModuleType())
+                {
+                    ResolverUtility.TryResolveSubmodule(submodulePropInfo, this.usedRegistry, this.module);
+                }
+            }
+        }
+
+        private bool TryResolveComponent(PropertyInfo propInfo)
+        {
+            bool resolved = true;
+
+            object currentValue = propInfo.GetValue(this.module, null);
+            if (currentValue != null)
+            {
+                resolved = false;
+            }
+            else
+            {
+                ResolveComponent(propInfo);
+            }
+
+            return resolved;
+        }
+
+        private void ResolveComponent(PropertyInfo propInfo)
+        {
+            string propName = propInfo.Name;
+
+            bool resolved = TryResolveComponentFromRegistry(propInfo);
+            if (!resolved)
+            {
+                ExceptionHelper.ThrowPropertyAndTypeException(this.module.GetType(), Errors.ModuleResolver_MissingPropertyRegistration, propName);
+            }
+        }
+
+        private bool TryResolveComponentFromRegistry(PropertyInfo propInfo)
+        {
+            Type propType = propInfo.PropertyType;
+
+            if (this.usedRegistry == null)
+            {
+                return false;
+            }
+
+            if (!this.usedRegistry.IsRegistered(propType))
+            {
+                return false;
+            }
+            var component = this.usedRegistry.GetComponent(propType);
+            this.moduleType.SetPropertyRecursive(this.module, propInfo.Name, component);
+            return true;
+        }
+    }
+}
